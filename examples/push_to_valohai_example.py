07f6ce0a8c2cccc8e81da4746a39f725312dd007
Ruksi <me@ruk.si>
2018-11-13 15:27:34 +0200

    Fix reference to old adapter module name in Valohai example

--- examples/push_to_valohai_example.py
+++ examples/push_to_valohai_example.py
@@ -1,16 +1,19 @@
-import sys, os
+import sys
 sys.path.append("../../valohai-sagemaker-adapter")
 
 from valohai_sagemaker.valohai import ValohaiAdapter, CodeContainer
-from valohai_sagemaker.shell import CommandRunner
 from valohai_sagemaker.path import module_rootdir
-
 
 if __name__ == "__main__":
     adapter = ValohaiAdapter(
-        CodeContainer("test-project",
-                      pip_packages=["imbalanced-learn"],
-                      files_to_copy=["../examples/jupyter-example/train.py", module_rootdir("docker_sagemaker_image_runner")]),
+        CodeContainer(
+            "test-project",
+            pip_packages=["imbalanced-learn"],
+            files_to_copy=[
+                "../examples/jupyter-example/train.py",
+                module_rootdir("valohai_sagemaker")
+            ]
+        ),
         dockerhub_image="ufoym/deepo:pytorch"
     )
     adapter.launch_execution({"training": "some/link/to/data/on/the/internet"})
